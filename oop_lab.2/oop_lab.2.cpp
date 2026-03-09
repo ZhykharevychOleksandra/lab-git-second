fa74a70cbfd38ea06944ae393693655d0167646a
Олександра Жихаревич <oleksandraziharevic@gmail.com>
2026-03-10 01:16:04 +0200

    Remove old oop.lab2 and full oop_lab.2 code

--- oop_lab.2/oop_lab.2.cpp
+++ oop_lab.2/oop_lab.2.cpp
@@ -1,20 +1,14 @@
-﻿// oop_lab.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include "Product.h"
+#include "Customer.h"
+#include "Order.h"
 
-#include <iostream>
+int main() {
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    Product p1(1, "Laptop", 25000);
+    Customer c1(2, "Sasha", "sasha@gmail.com");
+
+    Order o1(100, p1, c1, 2);
+    o1.printInfo();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
